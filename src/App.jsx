755bea2c8e808e29ee0f307be73acb48b06a03d5
ChristopherLit <litchristopher@gmail.com>
2024-01-10 17:13:10 -0500

    Fixed button positioning

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-
 import { useState } from 'react';
-import { motion } from "framer-motion"
+import { motion } from 'framer-motion';
 import './App.css';
 
 function App() {
-
   return (
-        <div className='game-layout'>
-        <h1 className='title'>BlackJack</h1>
-        <button className="Hit">Hit</button>
-        <button className="Stand">Stand</button>
-      </div>    
+    <div className='game-layout'>
+      <h1 className='title'>BlackJack</h1>
+
+      <motion.button
+        className='Hit'
+        whileHover={{ scale: 1.1 }}
+        initial={{ x: '60%', y: '300%' }}
+        whileTap={{ scale: 0.88 }}
+        onClick={() => null}
+      >
+        Hit
+      </motion.button>
+
+      <motion.button 
+      className='Stand'
+      whileHover={{ scale: 1.1 }}
+        initial={{ x: '-120%', y: '300%' }}
+        whileTap={{ scale: 0.88 }}
+        onClick={() => null}
+        >
+      Stand
+      </motion.button>
+    </div>
   );
 }
 
