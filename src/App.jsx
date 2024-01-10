9b6235e59bd94d18a6c409e0f03222e8947137f0
ChristopherLit <litchristopher@gmail.com>
2024-01-10 15:36:25 -0500

    Base UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,16 @@
 
-import "primereact/resources/themes/saga-green/theme.css";
-import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";
-
-import { Slider } from 'primereact/slider';
 import { useState } from 'react';
+import { motion } from "framer-motion"
 import './App.css';
 
 function App() {
-  const [value1, setValue1] = useState(null);
-  
 
   return (
-    <div className='game-container'>
-    <div className='container'>
-      <h1 className='title'>BlackJack</h1>
-      <button className="Hit">Hit</button>
-      <button className="Stand">Stand</button>
-    </div>
-
-    <div className='popup-container'>
-
-    <button className="Half">Half</button>
-      <button className="All-In">All In</button>
-      <button className="Deal">Deal</button>
-      <div style={{ padding: "10px", width: "110%" }}>
-        <p>${value1}</p>
-        <Slider value={value1} onChange={(e) => setValue1(e.value)} />
-      </div>
-
-    </div>
-  </div>
+        <div className='game-layout'>
+        <h1 className='title'>BlackJack</h1>
+        <button className="Hit">Hit</button>
+        <button className="Stand">Stand</button>
+      </div>    
   );
 }
 
